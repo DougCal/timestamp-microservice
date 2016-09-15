f3309dcd04a107fa0be24a088b0f4b42a61b8199
John <calhounj595@gmail.com>
2016-09-15 13:54:26 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.end('Hello World!');
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('LISTENING');
+});

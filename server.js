67684edaf93ffad7eeb8bdd9c8840efbb29ba3b6
John <calhounj595@gmail.com>
2016-09-16 18:51:34 +0000

    returns unix time or Gregorian MDY

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.end('Hello World!');
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/timestmp.html'));
+});
+
+app.get('/:id', function (req, res) {
+  
+  var time = req.params.id;
+  var obj;
+  
+  if(isNaN(time)){
+    var unixTime = Date.parse(time)/1000;
+    obj = {
+      'unix': unixTime,
+      'natural': time
+    };
+    
+  }else{
+    var monthNames = [ "January", "February", "March", "April", "May", "June", 
+                       "July", "August", "September", "October", "November", "December" ];
+    
+    var date = new Date(time * 1000);
+    var month = monthNames[date.getMonth()];
+    var day = date.getDate();
+    var year = date.getFullYear();
+    
+    obj = {
+      'unix': time,
+      'natural': month + " " + day + ", " + year
+    };
+  }
+  
+  res.send(obj);
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
